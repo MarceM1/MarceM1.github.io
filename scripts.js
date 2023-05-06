909422bded96c1c34e308fa9c8c90f49ced7f236
MarceM1 <117491210+MarceM1@users.noreply.github.com>
2023-05-06 01:08:25 -0300

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,37 @@
+window.sr = ScrollReveal();
+    sr.reveal(`.about__content`, {
+        duration: 3000,
+        origin: `top`,
+        distance:  `-50px`
+
+    });   
+    sr.reveal(`.about__picture`, {
+         duration: 3000,
+         origin: `bottom`,
+         distance:  `-50px`
+    });
+    sr.reveal(`.contact-container`, {
+        duration: 5000,
+    });   
+
+     sr.reveal(`.form-container`, {
+         duration: 3000,
+         origin: `right`,
+         distance:  `-500px`
+
+     });
+    sr.reveal(`.form-container h3`, {
+        duration: 3000,
+        origin: `bottom`,
+        distance:  `-100px`
+
+    });
+   
+    
+
+    sr.reveal(`.map`, {
+        duration: 3000,
+        origin: `left`,
+        distance:  `-500px`
+
+    });
